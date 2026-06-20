f7b5310e76654b6a19c9ab748b029e63d3419b6a
gargi763 <rgvdshinde@gmail.com>
2026-06-20 09:15:25 +0530

    Update studentData.ts

--- src/data/studentData.ts
+++ src/data/studentData.ts
@@ -1,6 +1,6 @@
 export const student = {
-  name: 'Amey Krishna Gapate',
-  email: 'amey.gapate@university.edu',
+  name: 'Ajay Jadeja',
+  email: 'ajay.jadeja@university.edu',
   id: 'STU-2024-0847',
   major: 'Computer Science',
   year: '3rd Year',
